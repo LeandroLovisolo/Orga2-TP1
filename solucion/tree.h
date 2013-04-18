5caa685883dcde868c600927a8bc3a09e15f56f1
Leandro Lovisolo <leandro@leandro.me>
2013-04-18 06:52:37 -0300

    Limpieza de tree.h

--- solucion/tree.h
+++ solucion/tree.h
@@ -8,20 +8,20 @@ extern "C" {
 
 typedef enum boolean
 {
-	False = 0,
-	True = 1
+    False = 0,
+    True = 1
 } boolean;
 
 typedef enum tree_type {
-	Integer,
-	Double,
-	String
+    Integer,
+    Double,
+    String
 } tree_type;
 
 typedef union tree_value {
-		int i;
-		double d;
-		char *s;
+    int i;
+    double d;
+    char *s;
 } __attribute__((__packed__)) tree_value;
 
 tree_value ival(int i);
@@ -31,21 +31,20 @@ tree_value sval(char *s);
 struct list_node;
 
 typedef struct tree {
-	
-	struct list_node *children;
-	tree_value value;
-	tree_type  type;
-	
+    struct list_node *children;
+    tree_value value;
+    tree_type  type;
+    
 } __attribute__((__packed__)) tree;
 
 typedef struct list_node {
-	tree *element;
-	struct list_node *next;
+    tree *element;
+    struct list_node *next;
 } __attribute__((__packed__)) list_node;
 
 typedef list_node* list_node_p;
 
-typedef boolean    (*tree_bool_method)(tree* self);
+typedef boolean (*tree_bool_method)(tree* self);
 typedef tree_value (*tree_value_method)(tree *parent, tree *child);
 
 tree* tree_create(tree_value value, tree_type type);
@@ -54,19 +53,14 @@ tree* tree_create_int(int value);
 tree* tree_create_double(double value);
 tree* tree_create_string(char *str);
 
-void  tree_deep_delete(tree *self);
+void tree_deep_delete(tree *self);
 
+void tree_add_child(tree *self, tree *element);
 
-boolean tree_has_childs(tree *self);
-void  tree_add_child(tree *self, tree *element);
-
-tree* tree_children_at(tree *self, int position);
-
-void  tree_prune(tree *self, tree_bool_method method);
+void tree_prune(tree *self, tree_bool_method method);
 void tree_merge(tree *self, tree_bool_method test_method, tree_value_method value_method);
 
 void tree_print(tree *self, char *extra, char *archivo);
-
 
 boolean es_bisiesto(tree *t);
 boolean es_mayor_que_sesenta(tree *t);
@@ -76,11 +70,8 @@ tree_value sumar(tree* padre, tree *hijo);
 tree_value multiplicar(tree* padre, tree *hijo);
 tree_value intercalar(tree* padre, tree *hijo);
 
-
 #ifdef __cplusplus
 }
 #endif
 
 #endif /// _TREE_H_
-
-
